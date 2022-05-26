51fb1bfbf82adb039d5f8c2a433a6ef1e65dcc87
oklays <okkyalwan7@gmail.com>
2022-05-26 17:04:34 +0700

    fix : Change display on dashboard screen

--- src/modules/Dashboard/Component/DashboardComponent.js
+++ src/modules/Dashboard/Component/DashboardComponent.js
@@ -1,7 +1,8 @@
+import { Table } from "antd";
 import React from "react";
 
 const DashboardComponent = (props) => {
-  const { userDetail } = props;
+  const { userDetail, headers, listServices } = props;
   return (
     <div>
       <div class="page-content">
@@ -273,162 +274,31 @@ const DashboardComponent = (props) => {
         </div>
 
         <div class="row">
-          <div class="col-lg-7 col-xl-8 grid-margin stretch-card">
+          <div class="col-lg-12 col-xl-12 grid-margin stretch-card">
             <div class="card">
               <div class="card-body">
                 <div class="d-flex justify-content-between align-items-baseline mb-2">
-                  <h6 class="card-title mb-0">Monthly sales</h6>
-                  <div class="dropdown mb-2">
-                    <button
-                      class="btn p-0"
-                      type="button"
-                      id="dropdownMenuButton4"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      <i
-                        class="icon-lg text-muted pb-3px"
-                        data-feather="more-horizontal"
-                      ></i>
-                    </button>
-                    <div
-                      class="dropdown-menu"
-                      aria-labelledby="dropdownMenuButton4"
-                    >
-                      <a
-                        class="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <i data-feather="eye" class="icon-sm mr-2"></i>{" "}
-                        <span class="">View</span>
-                      </a>
-                      <a
-                        class="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <i data-feather="edit-2" class="icon-sm mr-2"></i>{" "}
-                        <span class="">Edit</span>
-                      </a>
-                      <a
-                        class="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <i data-feather="trash" class="icon-sm mr-2"></i>{" "}
-                        <span class="">Delete</span>
-                      </a>
-                      <a
-                        class="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <i data-feather="printer" class="icon-sm mr-2"></i>{" "}
-                        <span class="">Print</span>
-                      </a>
-                      <a
-                        class="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <i data-feather="download" class="icon-sm mr-2"></i>{" "}
-                        <span class="">Download</span>
-                      </a>
-                    </div>
-                  </div>
+                  <h6 class="card-title mb-0">10 Service Terbaru</h6>
                 </div>
                 <p class="text-muted mb-4">
-                  Sales are activities related to selling or the number of goods
-                  or services sold in a given time period.
+                  Menampilkan 10 service berdasarkan tanggal terbaru
                 </p>
-                <div class="monthly-sales-chart-wrapper">
-                  <canvas id="monthly-sales-chart"></canvas>
+                {/* <div class="monthly-sales-chart-wrapper"> */}
+                {/* <canvas id="monthly-sales-chart"></canvas> */}
+                <div class="table-responsive">
+                  <Table
+                    columns={headers}
+                    dataSource={listServices}
+                    size={"middle"}
+                    pagination={{
+                      size: 3,
+                      pageSizeOptions: ["10", "20", "50"],
+                      showSizeChanger: true,
+                      // ...pagination,
+                    }}
+                  />
                 </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-5 col-xl-4 grid-margin stretch-card">
-            <div class="card">
-              <div class="card-body">
-                <div class="d-flex justify-content-between align-items-baseline mb-2">
-                  <h6 class="card-title mb-0">Cloud storage</h6>
-                  <div class="dropdown mb-2">
-                    <button
-                      class="btn p-0"
-                      type="button"
-                      id="dropdownMenuButton5"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      <i
-                        class="icon-lg text-muted pb-3px"
-                        data-feather="more-horizontal"
-                      ></i>
-                    </button>
-                    <div
-                      class="dropdown-menu"
-                      aria-labelledby="dropdownMenuButton5"
-                    >
-                      <a
-                        class="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <i data-feather="eye" class="icon-sm mr-2"></i>{" "}
-                        <span class="">View</span>
-                      </a>
-                      <a
-                        class="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <i data-feather="edit-2" class="icon-sm mr-2"></i>{" "}
-                        <span class="">Edit</span>
-                      </a>
-                      <a
-                        class="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <i data-feather="trash" class="icon-sm mr-2"></i>{" "}
-                        <span class="">Delete</span>
-                      </a>
-                      <a
-                        class="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <i data-feather="printer" class="icon-sm mr-2"></i>{" "}
-                        <span class="">Print</span>
-                      </a>
-                      <a
-                        class="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <i data-feather="download" class="icon-sm mr-2"></i>{" "}
-                        <span class="">Download</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div id="progressbar1" class="mx-auto"></div>
-                <div class="row mt-4 mb-3">
-                  <div class="col-6 d-flex justify-content-end">
-                    <div>
-                      <label class="d-flex align-items-center justify-content-end tx-10 text-uppercase font-weight-medium">
-                        Total storage{" "}
-                        <span class="p-1 ml-1 rounded-circle bg-primary-muted"></span>
-                      </label>
-                      <h5 class="font-weight-bold mb-0 text-right">8TB</h5>
-                    </div>
-                  </div>
-                  <div class="col-6">
-                    <div>
-                      <label class="d-flex align-items-center tx-10 text-uppercase font-weight-medium">
-                        <span class="p-1 mr-1 rounded-circle bg-primary"></span>{" "}
-                        Used storage
-                      </label>
-                      <h5 class="font-weight-bold mb-0">6TB</h5>
-                    </div>
-                  </div>
-                </div>
-                <button class="btn btn-primary btn-block">
-                  Upgrade storage
-                </button>
+                {/* </div> */}
               </div>
             </div>
           </div>
--- src/modules/Dashboard/Container/DashboardContainer.js
+++ src/modules/Dashboard/Container/DashboardContainer.js
@@ -1,22 +1,192 @@
+import { Space, Tag } from "antd";
 import React from "react";
 import { connect } from "react-redux";
 import { store } from "../../../app/ConfigureStore";
+import CButtonAntd from "../../../components/CButton/CButtonAntd";
 import { setGlobalLoading } from "../../App/Store/ComponentAction";
 import DashboardComponent from "../Component/DashboardComponent";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import * as ListServiceActions from "../../ListServices/Store/ListServicesActions";
+import Text from "antd/lib/typography/Text";
+import moment from "moment";
 
 const DashboardContainer = (props) => {
   const {
+    handlePressEdit,
+    handlePressDelete,
     auth: { userDetail },
+    services: { listServices, paging },
   } = props;
   React.useEffect(() => {
     store.dispatch(setGlobalLoading(false));
   }, []);
-  return <DashboardComponent userDetail={userDetail} />;
+
+  const renderActionTable = (text, record) => (
+    <Space size="middle">
+      <CButtonAntd
+        onClick={() => {
+          handlePressEdit(record);
+        }}
+        type="primary"
+        icon={<EditOutlined />}
+        size="middle"
+      />
+    </Space>
+  );
+
+  const switchColorType = (isExternal, isWarranty) => {
+    if (isWarranty) {
+      return "#ffc018";
+    }
+    switch (isExternal) {
+      case true:
+        return "#87d068";
+        break;
+
+      default:
+        return "#108ee9";
+        break;
+    }
+  };
+
+  const headers = [
+    {
+      title: "No",
+      dataIndex: "no",
+      key: "no",
+      width: "5%",
+      sorter: (a, b) => a.no - b.no,
+    },
+    {
+      title: "Tipe",
+      dataIndex: "type",
+      key: "type",
+      width: "15%",
+      render: (type, items) => {
+        let color = switchColorType(items.is_external, items.warranty);
+        return (
+          <Tag
+            style={{ borderRadius: 10, width: 115, textAlign: "center" }}
+            color={color}
+            key={type}
+          >
+            {type.toUpperCase()}
+          </Tag>
+        );
+      },
+      sorter: (a, b) => a.type.length - b.type.length,
+    },
+    {
+      title: "Customer",
+      dataIndex: "customer_name",
+      key: "customer_name",
+      width: "40%",
+      sorter: (a, b) => a.customer_name.length - b.customer_name.length,
+    },
+    {
+      title: "Teknisi",
+      dataIndex: "employees",
+      render: (employees) =>
+        employees.map((employee) => (
+          <Text>
+            {employee.employee_name}
+            {employees.length <= 1 ? "" : ", "}
+          </Text>
+        )),
+      key: "employees",
+      width: "15%",
+    },
+    {
+      title: "Unit",
+      dataIndex: "unit_models",
+      render: (units) =>
+        units.map((unit) => (
+          <Text>
+            {unit.unit_name}
+            {units.length <= 1 ? "" : ", "}
+          </Text>
+        )),
+      key: "unit_models",
+      width: "15%",
+    },
+    {
+      title: "Model",
+      dataIndex: "unit_models",
+      render: (units) =>
+        units.map((unit) => (
+          <Text>
+            {unit.unit_model_name}
+            {units.length <= 1 ? "" : ", "}
+          </Text>
+        )),
+      key: "unit_models",
+      width: "15%",
+      sorter: (a, b) => a.customer_name.length - b.customer_name.length,
+    },
+    {
+      title: "Due Date",
+      dataIndex: "due",
+      key: "due",
+      width: "15%",
+      sorter: (a, b) => a.due - b.due,
+      render: (due) => {
+        return <Text>{moment(due).format("YYYY-MM-DD")}</Text>;
+      },
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
+      render: (status) => {
+        let color = status === "Progress" ? "#108ee9" : "#54BAB9";
+        return (
+          <Tag
+            style={{ width: 90, textAlign: "center" }}
+            color={color}
+            key={status}
+          >
+            {status.toUpperCase()}
+          </Tag>
+        );
+      },
+      width: "15%",
+      sorter: (a, b) => a.status.length - b.status.length,
+    },
+    {
+      title: "Dibuat",
+      dataIndex: "created_date",
+      key: "created_date",
+      width: "15%",
+      sorter: (a, b) => a.created_date.length - b.created_date.length,
+    },
+    {
+      align: "center",
+      title: "Aksi",
+      key: "action",
+      width: "30%",
+      render: renderActionTable,
+    },
+  ];
+
+  return (
+    <DashboardComponent
+      userDetail={userDetail}
+      headers={headers}
+      listServices={listServices}
+    />
+  );
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
+  services: state.services,
 });
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  handlePressEdit: async (value) => ListServiceActions.handlePressEdit(value),
+  handlePressDelete: async (jobServiceId) => {
+    await dispatch(ListServiceActions.setSelectedJobServiceId(jobServiceId));
+    ListServiceActions.deleteJobServiceRequested(jobServiceId);
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(DashboardContainer);
